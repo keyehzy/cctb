d88eadfed4b54285dfb5778721cb341f9df4619e
Matheus S. M. de Sousa <msamuel@aluno.puc-rio.br>
2023-03-30 10:22:32 -0300

    We don't need RefCounted and RefPtr for now

---
+++ test/test-array.cpp
@@ -0,0 +1,18 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "LinearAlgebra/NumericArray.h"
+
+TEST_CASE("axpy", "[NumericArray]") {
+  NumericArray<double> x(3);
+  x[0] = 1.0;
+  x[1] = 2.0;
+  x[2] = 3.0;
+  NumericArray<double> y(3);
+  y[0] = 4.0;
+  y[1] = 5.0;
+  y[2] = 6.0;
+  axpy(2.0, x, y);
+  REQUIRE(y[0] == 6.0);
+  REQUIRE(y[1] == 9.0);
+  REQUIRE(y[2] == 12.0);
+}
