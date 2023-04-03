c761c47ef96c2491d105aa4b6f7e94b6c2356467
Matheus <keyehzy@proton.me>
2023-04-03 17:10:14 -0300

    Ship BLAS functions

--- src/LinearAlgebra/Matrix.cpp
+++ src/LinearAlgebra/Matrix.cpp
@@ -1,7 +1,15 @@
 #include "LinearAlgebra/Matrix.h"
 
+#if HAVE_BLAS
+#include "cblas.h"
+#endif
+
 void gemv(double alpha, const Matrix<double>& A, const NumericArray<double>& x, double beta,
           NumericArray<double>& y) {
+#if HAVE_BLAS
+  cblas_dgemv(CblasRowMajor, CblasNoTrans, A.rows(), A.cols(), alpha, A.buffer().data(), A.cols(),
+              x.buffer().data(), 1, beta, y.buffer().data(), 1);
+#else
   for (int i = 0; i < A.rows(); ++i) {
     double result = 0;
     for (int j = 0; j < A.cols(); ++j) {
@@ -9,19 +17,30 @@ void gemv(double alpha, const Matrix<double>& A, const NumericArray<double>& x, 
     }
     y[i] = alpha * result + beta * y[i];
   }
+#endif
 }
 
 void ger(double alpha, const NumericArray<double>& x, const NumericArray<double>& y,
          Matrix<double>& A) {
+#if HAVE_BLAS
+  cblas_dger(CblasRowMajor, A.rows(), A.cols(), alpha, x.buffer().data(), 1, y.buffer().data(), 1,
+             A.buffer().data(), A.cols());
+#else
   for (int i = 0; i < A.rows(); ++i) {
     for (int j = 0; j < A.cols(); ++j) {
       A(i, j) += alpha * x[i] * y[j];
     }
   }
+#endif
 }
 
 void gemm(double alpha, const Matrix<double>& a, const Matrix<double>& b, double beta,
           Matrix<double>& c) {
+#if HAVE_BLAS
+  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, a.rows(), b.cols(), a.cols(), alpha,
+              a.buffer().data(), a.cols(), b.buffer().data(), b.cols(), beta, c.buffer().data(),
+              c.cols());
+#else
   for (int i = 0; i < a.rows(); ++i) {
     for (int j = 0; j < b.cols(); ++j) {
       double result = 0;
@@ -31,4 +50,5 @@ void gemm(double alpha, const Matrix<double>& a, const Matrix<double>& b, double
       c(i, j) = alpha * result + beta * c(i, j);
     }
   }
+#endif
 }
